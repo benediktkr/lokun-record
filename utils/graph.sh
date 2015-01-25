abd9a195523bdfa915a31f1ffc8ba3b48ccf65c9
Benedikt Krisinsson <benedikt@lokun.is>
2015-01-25 02:38:10 +0000

    import data from monitor.log to rrdtool

---
+++ utils/graph.sh
@@ -0,0 +1,80 @@
+rrdtool graph usercount24h_graph.png \
+-w 785 -h 120 -a PNG \
+--slope-mode \
+--start -86400 --end now \
+--font DEFAULT:7: \
+--title "usercount 24h" \
+--watermark "`date`" \
+--vertical-label "usercount" \
+--lower-limit 0 \
+--right-axis 1:0 \
+--x-grid MINUTE:10:HOUR:1:MINUTE:120:0:%R \
+--alt-y-grid --rigid \
+DEF:usercount=monitor_db.rrd:bandwidth:MAX \
+LINE1:usercount#0000FF:"usercount" \
+GPRINT:usercount:LAST:"Cur\: %5.0lf" \
+GPRINT:usercount:AVERAGE:"Avg\: %5.0lf" \
+GPRINT:usercount:MAX:"Max\: %5.0lf" \
+GPRINT:usercount:MIN:"Min\: %5.0lf\t\t\t" \
+COMMENT:"comment\:" \
+
+rrdtool graph usercount3d_graph.png \
+-w 785 -h 120 -a PNG \
+--slope-mode \
+--start -259200 --end now \
+--font DEFAULT:7: \
+--title "usercount 3d" \
+--watermark "`date`" \
+--vertical-label "usercount" \
+--lower-limit 0 \
+--right-axis 1:0 \
+--x-grid MINUTE:60:HOUR:2:MINUTE:360:0:%R \
+--alt-y-grid --rigid \
+DEF:usercount=monitor_db.rrd:bandwidth:MAX \
+LINE1:usercount#0000FF:"usercount" \
+GPRINT:usercount:LAST:"Cur\: %5.0lf" \
+GPRINT:usercount:AVERAGE:"Avg\: %5.0lf" \
+GPRINT:usercount:MAX:"Max\: %5.0lf" \
+GPRINT:usercount:MIN:"Min\: %5.0lf\t\t\t" \
+COMMENT:"comment\:" \
+
+
+rrdtool graph bandwidth24h_graph.png \
+-w 785 -h 120 -a PNG \
+--slope-mode \
+--start -86400 --end now \
+--font DEFAULT:7: \
+--title "bandwidth 24h" \
+--watermark "`date`" \
+--vertical-label "bandwidth (mb/s)" \
+--lower-limit 0 \
+--right-axis 1:0 \
+--x-grid MINUTE:10:HOUR:1:MINUTE:120:0:%R \
+--alt-y-grid --rigid \
+DEF:bw=monitor_db.rrd:bandwidth:MAX \
+LINE1:bw#0000FF:"bandwidth" \
+GPRINT:bw:LAST:"Cur\: %5.2lf" \
+GPRINT:bw:AVERAGE:"Avg\: %5.2lf" \
+GPRINT:bw:MAX:"Max\: %5.2lf" \
+GPRINT:bw:MIN:"Min\: %5.2lf\t\t\t" \
+COMMENT:"comment\:" \
+
+rrdtool graph bandwidth3d_graph.png \
+-w 785 -h 120 -a PNG \
+--slope-mode \
+--start -259200 --end now \
+--font DEFAULT:7: \
+--title "bandwidth 3d" \
+--watermark "`date`" \
+--vertical-label "bandwidth" \
+--lower-limit 0 \
+--right-axis 1:0 \
+--x-grid MINUTE:60:HOUR:2:MINUTE:360:0:%R \
+--alt-y-grid --rigid \
+DEF:bandwidth=monitor_db.rrd:bandwidth:MAX \
+LINE1:bandwidth#0000FF:"bandwidth" \
+GPRINT:bandwidth:LAST:"Cur\: %5.2lf" \
+GPRINT:bandwidth:AVERAGE:"Avg\: %5.2lf" \
+GPRINT:bandwidth:MAX:"Max\: %5.2lf" \
+GPRINT:bandwidth:MIN:"Min\: %5.2lf\t\t\t" \
+COMMENT:"comment\:" \
---
+++ utils/monitorparser.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from time import mktime
+from commands import getoutput
+from collections import defaultdict
+
+def getdate(line):
+    return line.split('\t')[0]
+
+def gettimestamp(date):
+    t = mktime(datetime.strptime(date, "%Y-%m-%d %H:%M:%S" ).timetuple())
+    return int(t)
+
+def rrdupdate(ts, usercount, bw):
+    rrdupdate = "rrdtool update monitor_db.rrd --template bandwidth:usercount {0}:{1}:{2}"
+    r = rrdupdate.format(ts, bw, usercount) 
+    #print r
+    rrd = getoutput(r)
+    if rrd.startswith("ERROR"):
+        print rrd
+    return rrd
+
+def parseline(line):
+    date, text = line.split('\t')
+    ts = gettimestamp(date)
+    return (ts, text)
+
+if __name__ == "__main__":
+    with open('monitor.log', 'r') as f:
+        monitor = f.readlines()
+
+    print "preprocessing"
+    pre = list()
+    for i in xrange(len(monitor)):
+        line = monitor[i]
+        nextline = monitor[i+1] if i+1 < len(monitor) else ""
+            
+        try:
+            ts, msg = parseline(line)
+            nextts, nextmsg = parseline(nextline)
+        except ValueError:
+            continue
+            
+        if msg.startswith("Usercount:"):
+            usercount = int(msg.split(": ")[1])
+            if nextmsg.startswith("Bandwidth usage: "):
+                bw = float(nextmsg.split(": ")[1].split(" ")[0])
+            else:
+                bw = 0
+            pre.append((ts, usercount, bw))
+
+
+    print "rrdtool..."
+    for value in pre:
+        rrdupdate(*value)
+        
